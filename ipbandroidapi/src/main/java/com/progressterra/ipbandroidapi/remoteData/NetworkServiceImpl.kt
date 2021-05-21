c6fa18c5d99b68910d05c425c43527285b3b317f
Ilya Stupak <>
2021-05-21 14:44:51 +0300

    уход от buildconfig

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/remoteData/NetworkServiceImpl.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/remoteData/NetworkServiceImpl.kt
@@ -1,6 +1,5 @@
 package com.progressterra.ipbandroidapi.remoteData
 
-import com.progressterra.ipbandroidapi.BuildConfig
 import com.progressterra.ipbandroidapi.interfaces.internal.NetworkService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -10,36 +9,25 @@ import java.util.concurrent.TimeUnit
 
 internal class NetworkServiceImpl : NetworkService {
 
-    private val accessKey: String = BuildConfig.ACCESS_KEY
-    private val baseUrl: String = BuildConfig.API_URL
-
-
     private val clientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
-        .callTimeout(TIMEOUT, TimeUnit.MINUTES)
-        .connectTimeout(TIMEOUT, TimeUnit.MINUTES)
-        .readTimeout(TIMEOUT, TimeUnit.MINUTES)
-        .writeTimeout(TIMEOUT, TimeUnit.MINUTES)
+        .callTimeout(NetworkSettings.NETWORK_CALL_TIMEOUT, TimeUnit.MILLISECONDS)
+        .connectTimeout(NetworkSettings.NETWORK_CONNECT_TIMEOUT, TimeUnit.MILLISECONDS)
+        .readTimeout(NetworkSettings.NETWORK_READ_TIMEOUT, TimeUnit.MILLISECONDS)
+        .writeTimeout(NetworkSettings.NETWORK_WRITE_TIMEOUT, TimeUnit.MILLISECONDS)
         .addInterceptor(HttpLoggingInterceptor().apply { setLevel(HttpLoggingInterceptor.Level.BODY) })
         .addInterceptor {
             val request = it.request().newBuilder()
-                .addHeader("AccessKey", accessKey)
+                .addHeader("AccessKey", NetworkSettings.ACCESS_KEY)
                 .build()
             it.proceed(request)
         }
 
     private val retrofitBuilder = Retrofit.Builder()
-        .baseUrl(baseUrl)
+        .baseUrl(NetworkSettings.LIKEDISLIKE_ROOT_URL)
         .client(clientBuilder.build())
         .addConverterFactory(GsonConverterFactory.create())
 
     private var retrofit: Retrofit = retrofitBuilder.build()
 
     override fun <T> createService(apiClass: Class<T>): T = retrofit.create(apiClass)
-
-    companion object {
-
-        // Таймаут запроса в минутах
-        private const val TIMEOUT = 1L
-
-    }
 }
---
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/remoteData/NetworkSettings.kt
@@ -0,0 +1,60 @@
+package com.progressterra.ipbandroidapi.remoteData
+
+object NetworkSettings {
+    /**
+     * Задержка между событиями кнопки выход, мс
+     */
+    const val EXIT_TIMEOUT = 1000
+
+    /**
+     * Время вызова
+     */
+    const val NETWORK_CALL_TIMEOUT: Long = 20 * 1000
+
+    /**
+     * Время подключения
+     */
+    const val NETWORK_CONNECT_TIMEOUT: Long = 20 * 1000
+
+    /**
+     * Время чтения пакета
+     */
+    const val NETWORK_READ_TIMEOUT: Long = 20 * 1000
+
+    /**
+     * Время чтения пакета
+     */
+    const val NETWORK_WRITE_TIMEOUT: Long = 20 * 1000
+
+    /**
+     * Ключ доступа
+     */
+    var ACCESS_KEY = ""
+
+    /**
+     * Стандартная базовая ссылка (возможно потом поменяем на var)
+     */
+    const val LIKEDISLIKE_ROOT_URL = "http://ibonus.1c-work.net/"
+
+    /**
+     * Доп адреса для IBONUS
+     */
+    val LIKEDISLIKE_ADDITIONAL_URL = listOf<String>()
+
+    const val SCRM_ROOT_URL = "http://scrm.1c-work.net"
+
+    /**
+     * Доп адреса для SCRM
+     */
+    val SCRM_ADDITIONAL_URL = listOf<String>()
+
+    /**
+     * Использовать новый апи для каталога
+     */
+    const val DELIVERY_KEY_CDEK = "22fcfd37-36cd-4f2a-983b-1885d6c01ad8"
+
+    /**
+     * Использовать новый апи для каталога
+     */
+    const val DELIVERY_KEY_DPD = "411c31ae-5dfc-11ea-9a76-0200ac100a02"
+}
