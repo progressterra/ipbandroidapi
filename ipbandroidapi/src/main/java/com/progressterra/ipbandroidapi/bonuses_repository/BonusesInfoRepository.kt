090f985b00969f28e13b54f8fd3704fc49f4a2c3
Ilya Stupak <>
2021-05-21 11:44:48 +0300

    Добавлены запросы к серверу.

---
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/bonuses_repository/BonusesInfoRepository.kt
@@ -0,0 +1,10 @@
+package com.progressterra.ipbandroidapi.bonuses_repository
+
+import com.progressterra.ipbandroidapi.repository.models.access_token.AccessTokenResponse
+import com.progressterra.ipbandroidapi.repository.models.base.ResponseWrapper
+import com.progressterra.ipbandroidapi.repository.models.bonuses_info.GeneralInfoResponse
+
+internal interface BonusesInfoRepository {
+    suspend fun getAccessToken(): ResponseWrapper<AccessTokenResponse>
+    suspend fun getBonusesInfo(accessToken: String): ResponseWrapper<GeneralInfoResponse>
+}
--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/repository/RepositoryImpl.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/repository/RepositoryImpl.kt
@@ -3,6 +3,10 @@ package com.progressterra.ipbandroidapi.repository
 import com.progressterra.ipbandroidapi.di.DaggerRepositoryComponent
 import com.progressterra.ipbandroidapi.interfaces.internal.NetworkService
 import com.progressterra.ipbandroidapi.interfaces.internal.Repository
+import com.progressterra.ipbandroidapi.repository.models.access_token.AccessTokenRequest
+import com.progressterra.ipbandroidapi.repository.models.access_token.AccessTokenResponse
+import com.progressterra.ipbandroidapi.repository.models.bonuses_info.GeneralInfoResponse
+import retrofit2.Response
 import javax.inject.Inject
 
 internal class RepositoryImpl : Repository {
@@ -14,6 +18,14 @@ internal class RepositoryImpl : Repository {
         DaggerRepositoryComponent.builder()
             .build()
             .inject(this)
+    }
+
+    override suspend fun getAccessToken(accessToken: AccessTokenRequest): Response<AccessTokenResponse> {
+        return networkService.createService(Repository::class.java).getAccessToken(accessToken)
+    }
+
+    override suspend fun getGeneralInfo(accessToken: String): Response<GeneralInfoResponse> {
+        return networkService.createService(Repository::class.java).getGeneralInfo(accessToken)
     }
 
     // TODO: 14.05.2021 private val bonusApi  = networkService.createService(bonusapimodel)
