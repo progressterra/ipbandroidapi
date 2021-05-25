60002e42ebf556fc1ff9ef598bc6b6169697e0a9
Ilya Stupak <>
2021-05-25 12:27:01 +0300

    Правки возвращаемого значения

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/interfaces/client/bonuses/BonusesApi.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/interfaces/client/bonuses/BonusesApi.kt
@@ -1,5 +1,6 @@
 package com.progressterra.ipbandroidapi.interfaces.client.bonuses
 
+import com.progressterra.ipbandroidapi.bonuses_repository.BonusesInfo
 import com.progressterra.ipbandroidapi.remoteData.models.base.ResponseWrapper
 import com.progressterra.ipbandroidapi.remoteData.scrm.models.responses.AccessTokenResponse
 import com.progressterra.ipbandroidapi.remoteData.scrm.models.responses.GeneralInfoResponse
@@ -7,7 +8,7 @@ import com.progressterra.ipbandroidapi.remoteData.scrm.models.responses.GeneralI
 interface BonusesApi {
 
     suspend fun getAccessToken(): ResponseWrapper<AccessTokenResponse>
-    suspend fun getBonusesInfo(accessToken: String): ResponseWrapper<GeneralInfoResponse>
+    suspend fun getBonusesInfo(accessToken: String): ResponseWrapper<BonusesInfo>
 
     companion object {
         fun getInstance(): BonusesApi {
