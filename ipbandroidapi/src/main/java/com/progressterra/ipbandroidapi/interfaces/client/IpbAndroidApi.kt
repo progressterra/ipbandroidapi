4813a264b397aa6544331b99c79647d15ba0f982
Ilya Stupak <>
2021-06-02 18:21:36 +0300

    Добавлен accesskey в инициализацию

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/interfaces/client/IpbAndroidApi.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/interfaces/client/IpbAndroidApi.kt
@@ -5,14 +5,16 @@ import com.chibatching.kotpref.Kotpref
 import com.chibatching.kotpref.gsonpref.gson
 import com.google.gson.Gson
 import com.progressterra.ipbandroidapi.localdata.shared_pref.UserData
+import com.progressterra.ipbandroidapi.remoteData.NetworkSettings
 import com.progressterra.ipbandroidapi.utils.IdentUtils
 
 interface IpbAndroidApi {
 
     companion object {
-        fun init(context: Context) {
+        fun init(context: Context, accessKey: String) {
             UserData.androidId = IdentUtils.getAndroidId(context)
             Kotpref.gson = Gson()
+            NetworkSettings.ACCESS_KEY = accessKey
         }
 
         fun getSavedAccessToken() = UserData.accessToken
