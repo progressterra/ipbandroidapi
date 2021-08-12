6435b427e9c0d6c96494f991314adf9a0efb71c0
Ilya Stupak <>
2021-08-12 15:00:11 +0300

    Убраны лишние аннотации

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/interfaces/client/addresses/AddressApi.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/interfaces/client/addresses/AddressApi.kt
@@ -12,20 +12,16 @@ interface AddressApi {
 
     suspend fun getAddressList(accessToken: String): ResponseWrapper<ListOfAddressesResponse>
 
-
     suspend fun addClientAddress(
         accessToken: String,
         addressUI: AddressUI
     ): ResponseWrapper<ResultResponse>
 
-    @PUT("/api/v3/addressclient/{AccessToken}")
     suspend fun updateClientAddress(
         accessToken: String,
         addressUI: AddressUI
     ): ResponseWrapper<ResultResponse>
 
-    @Headers("Authorization: Token 444220bc01c3cb6a7bd102bdfb72175a2deee88e")
-    @POST("suggestions/api/4_1/rs/suggest/address")
     suspend fun getSuggestionsAddressFromDadata(query: String): ResponseWrapper<DadataSuggestionResponse>
 }
 
