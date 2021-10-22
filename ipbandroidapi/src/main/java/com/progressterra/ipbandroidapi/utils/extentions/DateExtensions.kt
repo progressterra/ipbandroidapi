2c495f6143b710849371e2f5a065d088d478e64f
ToMaTo_0 <47805315+IncredibleHohol@users.noreply.github.com>
2021-10-22 13:06:39 +0300

    timeZone update

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/utils/extentions/DateExtensions.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/utils/extentions/DateExtensions.kt
@@ -4,12 +4,31 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-private const val serverDateFormat = "yyyy-MM-dd'T'hh:mm:ss"
+private const val serverDateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+private const val serverDateFormatTimeZone = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSXXX"
 
 private val serverTimeZone = TimeZone.getTimeZone("Europe/Moscow")
 
 fun String?.parseToDate(): Date? {
-    val sdf = SimpleDateFormat(serverDateFormat, Locale.getDefault()).apply {
+    if (this == null) return null
+
+    val timeFormat = if (contains("+") || contains("-")) {
+        when {
+            indexOf("+") == -1 -> {
+                serverDateFormat
+            }
+            indexOf("-") == -1 -> {
+                serverDateFormat
+            }
+            else -> {
+                serverDateFormatTimeZone
+            }
+        }
+    } else {
+        serverDateFormatTimeZone
+    }
+
+    val sdf = SimpleDateFormat(timeFormat, Locale.getDefault()).apply {
         timeZone = serverTimeZone
     }
     return tryOrNull { sdf.parse(this) }
