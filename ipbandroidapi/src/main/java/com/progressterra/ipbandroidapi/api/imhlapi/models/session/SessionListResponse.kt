e2c8a1d66111651a7718735571c7244684edc0c6
kmem1 <konstantinovich.aleks@mail.ru>
2022-02-15 17:22:54 +0300

    Make models classes outer in Imhlapi

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/api/imhlapi/models/session/SessionListResponse.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/api/imhlapi/models/session/SessionListResponse.kt
@@ -7,31 +7,31 @@ import com.progressterra.ipbandroidapi.remoteData.models.base.BaseResponse
 data class SessionListResponse(
     @SerializedName("dataList")
     val dataList: List<SessionExt?>?
-): BaseResponse() {
-    data class SessionExt(
-        @SerializedName("dateAdded")
-        val dateAdded: String?,
-        @SerializedName("dateSoftRemoved")
-        val dateSoftRemoved: String?,
-        @SerializedName("dateUpdate")
-        val dateUpdate: String?,
-        @SerializedName("description")
-        val description: String?,
-        @SerializedName("idEnterprise")
-        val idEnterprise: String?,
-        @SerializedName("idQuantumEnd")
-        val idQuantumEnd: String?,
-        @SerializedName("idQuantumStart")
-        val idQuantumStart: String?,
-        @SerializedName("idUnique")
-        val idUnique: String?,
-        @SerializedName("idrfActor")
-        val idrfActor: String?,
-        @SerializedName("sessionType")
-        val sessionType: Int?,
-        @SerializedName("title")
-        val title: String?,
-        @SerializedName("titleAvatar")
-        val titleAvatar: String?
-    )
-}
+) : BaseResponse()
+
+data class SessionExt(
+    @SerializedName("dateAdded")
+    val dateAdded: String?,
+    @SerializedName("dateSoftRemoved")
+    val dateSoftRemoved: String?,
+    @SerializedName("dateUpdate")
+    val dateUpdate: String?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("idEnterprise")
+    val idEnterprise: String?,
+    @SerializedName("idQuantumEnd")
+    val idQuantumEnd: String?,
+    @SerializedName("idQuantumStart")
+    val idQuantumStart: String?,
+    @SerializedName("idUnique")
+    val idUnique: String?,
+    @SerializedName("idrfActor")
+    val idrfActor: String?,
+    @SerializedName("sessionType")
+    val sessionType: Int?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("titleAvatar")
+    val titleAvatar: String?
+)
