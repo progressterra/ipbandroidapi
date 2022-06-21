f65b03063c2138382ee9e2d5d4df06afee3f6a0f
velvet <berhram@protonmail.com>
2022-06-21 03:45:29 +0300

    feat: sort types

---
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/api/filter/types/SortType.kt
@@ -0,0 +1,7 @@
+package com.progressterra.ipbandroidapi.api.filter.types
+
+enum class SortType(val value: String) {
+    PRICE("price"),
+    NEW_COLLECTION("newCollection"),
+    DISCOUNT("discount")
+}
