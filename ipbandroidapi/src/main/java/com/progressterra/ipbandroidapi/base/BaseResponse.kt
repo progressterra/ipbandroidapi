e06ec9d52efd21ace9ddad6006371e7bb00cb6fc
velvet <berhram@protonmail.com>
2022-09-12 15:26:40 +0300

    feat: missing repos, di, urls etc

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/base/BaseResponse.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/base/BaseResponse.kt
@@ -2,7 +2,7 @@ package com.progressterra.ipbandroidapi.base
 
 import com.google.gson.annotations.SerializedName
 
-internal open class BaseResponse {
+internal open  class BaseResponse {
     @SerializedName("result", alternate = ["resultOperation"])
     val result: ResultResponse? = null
 
