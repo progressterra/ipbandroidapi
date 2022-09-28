90189ce79d6c3181525142a4dae243e1e56d0e1f
velvet <berhram@protonmail.com>
2022-09-28 14:10:16 +0300

    fix: formatting

--- ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/user/UserData.kt
+++ ipbandroidapi/src/main/java/com/progressterra/ipbandroidapi/user/UserData.kt
@@ -8,7 +8,6 @@ import com.progressterra.ipbandroidapi.api.scrm.model.ClientData
 @Suppress("unused")
 object UserData : KotprefModel() {
 
-    var androidId by stringPref("")
     var deviceId by stringPref("")
     var clientExist by booleanPref(false)
     var phone by stringPref("")
