2707a454968690d3e969c7e1738483a88f9e6a92
velvet <berhram@protonmail.com>
2022-10-07 07:02:40 +0300

    fix: error debbuging

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/core/AbstractCloudDataSource.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/core/AbstractCloudDataSource.kt
@@ -1,14 +1,19 @@
 package com.progressterra.ipbandroidapi.core
 
+import android.util.Log
 import com.progressterra.ipbandroidapi.exception.HandleException
 
 abstract class AbstractCloudDataSource(
     private val handleException: HandleException
 ) {
 
+    //TODO remove error print
+
     protected suspend fun <T> handle(block: suspend () -> T): T = try {
         block.invoke()
     } catch (error: Exception) {
+        error.printStackTrace()
+        Log.e("Cloud data source:", error.message ?: "no message")
         throw handleException.handle(error)
     }
 }
--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/core/AbstractRepository.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/core/AbstractRepository.kt
@@ -2,9 +2,11 @@ package com.progressterra.ipbandroidapi.core
 
 abstract class AbstractRepository {
 
+    //TODO remove error print
     protected suspend fun <T> handle(block: suspend () -> T): Result<T> = try {
         Result.success(block.invoke())
     } catch (exception: Exception) {
+        exception.printStackTrace()
         Result.failure(exception)
     }
 }
