4527c237ee0b1cd21ba94a2056029bbecf444676
velvet <berhram@protonmail.com>
2022-10-26 21:04:25 +0300

    fix: time default const

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/Constants.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/Constants.kt
@@ -2,7 +2,7 @@ package com.progressterra.ipbandroidapi
 
 object Constants {
 
-    const val DEFAULT_ID = "00000000-0000-0000-0000-000000000000"
+    const val EMPTY_ID = "00000000-0000-0000-0000-000000000000"
 
     const val EMPTY_DATE = "0000-00-00'T'00:00:00"
 }
