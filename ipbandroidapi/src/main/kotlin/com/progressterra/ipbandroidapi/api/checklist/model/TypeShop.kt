fcd92ca1e2256838c00509cf8c8064c380eef685
velvet <berhram@protonmail.com>
2022-11-16 09:28:09 +0300

    fix: formatting

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/checklist/model/TypeShop.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/checklist/model/TypeShop.kt
@@ -1,9 +1,5 @@
 package com.progressterra.ipbandroidapi.api.checklist.model
 
-/**
- *
- * Values: _0,_1,_2
- */
 enum class TypeShop(val value: Int) {
-    _0(0), _1(1), _2(2)
+    ZERO(0), ONE(1), TWO(2)
 }
