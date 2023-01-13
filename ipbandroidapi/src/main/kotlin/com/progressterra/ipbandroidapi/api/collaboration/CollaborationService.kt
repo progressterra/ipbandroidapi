e8f07f20186851fa62d94a8d1a48790d29fa1159
velvet <berhram@protonmail.com>
2023-01-13 14:20:29 +0300

    fix: retrofit annotations

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/collaboration/CollaborationService.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/collaboration/CollaborationService.kt
@@ -5,29 +5,38 @@ import com.progressterra.ipbandroidapi.api.collaboration.models.ResultListEnterp
 import com.progressterra.ipbandroidapi.api.collaboration.models.ResultListShop
 import com.progressterra.ipbandroidapi.api.collaboration.models.ResultOfferList
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CollaborationService {
 
     @GET("/mobile/enterprise/list/{AccessToken}/{latitude}/{longitude}")
     suspend fun partners(
-        accessToken: String, latitude: String, longitude: String
+        @Path("AccessToken") accessToken: String,
+        @Path("latitude") latitude: String,
+        @Path("longitude") longitude: String
     ): ResultListEnterpriseData
 
     @GET("/collaboration/complace/list/{AccessToken}/{latitude}/{longitude}/{IDEnterprise}")
     suspend fun organizationShops(
-        accessToken: String, latitude: String, longitude: String, organizationId: String
+        @Path("AccessToken") accessToken: String,
+        @Path("latitude") latitude: String,
+        @Path("longitude") longitude: String,
+        @Path("IDEnterprise") organizationId: String
     ): ResultListShop
 
     @GET("/mobile/enterprise/{AccessToken}/{latitude}/{longitude}/{IDEnterprise}")
     suspend fun organizationById(
-        accessToken: String, latitude: String, longitude: String, organizationId: String
+        @Path("AccessToken") accessToken: String,
+        @Path("latitude") latitude: String,
+        @Path("longitude") longitude: String,
+        @Path("IDEnterprise") organizationId: String
     ): ResultEnterpriseData
 
     @GET("/collaboration/offers/mobile/list/{AccessToken}/{latitude}/{longitude}/{IDEnterprise}")
     suspend fun offersByOrganization(
-        accessToken: String,
-        latitude: String,
-        longitude: String,
-        organizationId: String
+        @Path("AccessToken") accessToken: String,
+        @Path("latitude") latitude: String,
+        @Path("longitude") longitude: String,
+        @Path("IDEnterprise") organizationId: String
     ): ResultOfferList
 }
