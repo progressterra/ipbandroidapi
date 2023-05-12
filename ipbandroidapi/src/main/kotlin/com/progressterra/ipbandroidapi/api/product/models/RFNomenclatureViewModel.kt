afeaa4fa7e4f6023e7e848d3f36a2f85f887f1e1
velvet <berhram@protonmail.com>
2023-05-12 14:59:04 +0300

    feat: model new field

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/product/models/RFNomenclatureViewModel.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/product/models/RFNomenclatureViewModel.kt
@@ -22,48 +22,40 @@ import com.google.gson.annotations.SerializedName
 data class RFNomenclatureViewModel(
 
     /* Идентификатор спецификации, которой принадлежит данная номенклатура */
-    @SerializedName("idrfSpecification")
-    val idrfSpecification: String? = null,
+    @SerializedName("idrfSpecification") val idrfSpecification: String? = null,
 
     /* Наиименование - может задаваться вручную или генерироваться на основе правил */
-    @SerializedName("name")
-    val name: String? = null,
+    @SerializedName("name") val name: String? = null,
+
+    /* Подробное описание товара, используемое для презентации товара */
+    @SerializedName("commerseDescription") val commerseDescription: String? = null,
 
     /* Уникальный идентификатор */
-    @SerializedName("idUnique")
-    val idUnique: String? = null,
+    @SerializedName("idUnique") val idUnique: String? = null,
 
     /* Идентификатор организации, которой принадлежит данная запись */
-    @SerializedName("idEnterprise")
-    val idEnterprise: String? = null,
+    @SerializedName("idEnterprise") val idEnterprise: String? = null,
 
     /* Дата добавления записи */
-    @SerializedName("dateAdded")
-    val dateAdded: String? = null,
+    @SerializedName("dateAdded") val dateAdded: String? = null,
 
     /* Дата обновления записи */
-    @SerializedName("dateUpdated")
-    val dateUpdated: String? = null,
+    @SerializedName("dateUpdated") val dateUpdated: String? = null,
 
     /* Если дата установлена, то сущность помечена на удаление. Физически данные не удаляются  Функции Delete лишь помечают сущность на удаление устанавливая дату вызова метода */
-    @SerializedName("dateSoftRemoved")
-    val dateSoftRemoved: String? = null,
+    @SerializedName("dateSoftRemoved") val dateSoftRemoved: String? = null,
 
     /* Список категорий в которых присутствует номенклатура */
-    @SerializedName("listCatalogCategory")
-    val listCatalogCategory: List<String>? = null,
+    @SerializedName("listCatalogCategory") val listCatalogCategory: List<String>? = null,
 
     /* Список изображений */
-    @SerializedName("listImages")
-    val listImages: List<RGEntitytMediaDataViewModel>? = null,
+    @SerializedName("listImages") val listImages: List<RGEntitytMediaDataViewModel>? = null,
 
     /* Порядок вывода товара по популярности */
-    @SerializedName("popularOrder")
-    val popularOrder: Int? = null,
+    @SerializedName("popularOrder") val popularOrder: Int? = null,
 
     /* Рейтинг товара */
-    @SerializedName("rating")
-    val rating: Double? = null
+    @SerializedName("rating") val rating: Double? = null
 
 )
 
