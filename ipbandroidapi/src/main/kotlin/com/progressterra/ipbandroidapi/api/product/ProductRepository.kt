9bc2824a95f2fde668228a667f0b9ef41a61e2b6
velvet <berhram@protonmail.com>
2023-05-15 09:31:25 +0300

    fix: product api

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/product/ProductRepository.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/product/ProductRepository.kt
@@ -8,38 +8,53 @@ import com.progressterra.ipbandroidapi.core.HandleException
 
 interface ProductRepository {
 
-    suspend fun productList(filterAndSort: FilterAndSort): Result<List<ProductView>?>
+    suspend fun productList(
+        accessToken: String,
+        filterAndSort: FilterAndSort
+    ): Result<List<ProductView>?>
 
-    suspend fun productByNomenclatureId(id: String): Result<List<ProductView>?>
+    suspend fun productByNomenclatureId(accessToken: String, id: String): Result<List<ProductView>?>
 
-    suspend fun productByGoodsInventoryId(id: String): Result<List<ProductView>?>
+    suspend fun productByGoodsInventoryId(
+        accessToken: String,
+        id: String
+    ): Result<List<ProductView>?>
 
 
     class Base(
         handleException: HandleException, private val service: ProductService
     ) : AbstractRepository(handleException), ProductRepository {
 
-        override suspend fun productList(filterAndSort: FilterAndSort): Result<List<ProductView>?> =
+        override suspend fun productList(
+            accessToken: String,
+            filterAndSort: FilterAndSort
+        ): Result<List<ProductView>?> =
             handle {
-                val response = service.productList(filterAndSort)
+                val response = service.productList(accessToken, filterAndSort)
                 if (response.result?.status != "success") {
                     throw BadRequestException()
                 }
                 response.dataList
             }
 
-        override suspend fun productByNomenclatureId(id: String): Result<List<ProductView>?> =
+        override suspend fun productByNomenclatureId(
+            accessToken: String,
+            id: String
+        ): Result<List<ProductView>?> =
             handle {
-                val response = service.productByNomenclatureId(id)
+                val response = service.productByNomenclatureId(accessToken, id)
                 if (response.result?.status != "success") {
                     throw BadRequestException()
                 }
                 response.dataList
             }
 
-        override suspend fun productByGoodsInventoryId(id: String): Result<List<ProductView>?> =
+        override suspend fun productByGoodsInventoryId(
+            accessToken: String,
+            id: String
+        ): Result<List<ProductView>?> =
             handle {
-                val response = service.productByGoodsInventoryId(id)
+                val response = service.productByGoodsInventoryId(accessToken, id)
                 if (response.result?.status != "success") {
                     throw BadRequestException()
                 }
--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/product/ProductService.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/product/ProductService.kt
@@ -4,17 +4,27 @@ import com.progressterra.ipbandroidapi.api.product.models.FilterAndSort
 import com.progressterra.ipbandroidapi.api.product.models.ProductViewResultDataList
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ProductService {
 
     @POST("/product/list")
-    suspend fun productList(@Body filterAndSort: FilterAndSort): ProductViewResultDataList
+    suspend fun productList(
+        @Header("AccessToken") accessToken: String,
+        @Body filterAndSort: FilterAndSort
+    ): ProductViewResultDataList
 
     @GET("/product/{idRFNomenclature}")
-    suspend fun productByNomenclatureId(@Query("idRFNomenclature") id: String): ProductViewResultDataList
+    suspend fun productByNomenclatureId(
+        @Header("AccessToken") accessToken: String,
+        @Query("idRFNomenclature") id: String
+    ): ProductViewResultDataList
 
     @GET("/product/rggoodsinventory/{idrggoodsinventory}")
-    suspend fun productByGoodsInventoryId(@Query("idrggoodsinventory") id: String): ProductViewResultDataList
+    suspend fun productByGoodsInventoryId(
+        @Header("AccessToken") accessToken: String,
+        @Query("idrggoodsinventory") id: String
+    ): ProductViewResultDataList
 }
