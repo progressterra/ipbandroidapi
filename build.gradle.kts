d471f9c01caa136ed05df6045e8b5f30aaf3a08d
velvet <berhram@protonmail.com>
2023-05-30 17:53:30 +0300

    build: ver bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
 allprojects {
     group = "com.progressterra.ipbandroidapi"
-    version = "0.5.9"
+    version = "0.5.10"
     repositories {
         google()
         mavenCentral()
