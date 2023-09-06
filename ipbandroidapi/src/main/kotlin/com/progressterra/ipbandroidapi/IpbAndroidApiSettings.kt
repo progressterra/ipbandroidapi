ed543c9e9ab1f054fb89b40efa94b661c0f3f0d6
velvet <berhram@proton.me>
2023-09-06 20:27:16 +0300

    feat: configurable ulr

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/IpbAndroidApiSettings.kt
+++ ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/IpbAndroidApiSettings.kt
@@ -2,10 +2,27 @@ package com.progressterra.ipbandroidapi
 
 object IpbAndroidApiSettings {
 
-    /**
-     * Ключ доступа
-     */
     var ACCESS_KEY = ""
 
     var DEBUG = false
+
+    var PRODUCT_URL = "http://51.250.54.134:7023/"
+
+    var DOCS_URL = "http://51.250.54.134:7107/"
+
+    var CATALOG_URL = "http://51.250.54.134:7027/"
+
+    var CART_URL = "http://51.250.54.134:7025/"
+
+    var BALANCE_URL = "http://51.250.54.134:7009/"
+
+    var MESSENGER_URL = "http://51.250.54.134:7093/"
+
+    var PAYMENT_DATA_URL = "http://51.250.54.134:7095/"
+
+    var PAYMENT_URL = "http://51.250.54.134:7097/"
+
+    var SCRM_URL = "http://51.250.54.134:7021/"
+
+    var AUTH_URL = "http://51.250.54.134:7001/"
 }
