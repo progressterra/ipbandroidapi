a462f780a4812c0c225d1c0fdd6524a95845a059
velvet <berhram@proton.me>
2023-09-06 17:32:26 +0300

    feat: deleted promocode service

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/ipbpromocode/IPBPromoCodeRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.progressterra.ipbandroidapi.api.ipbpromocode
-
-interface IPBPromoCodeRepository {
-
-    suspend fun setPromoCode(accessToken: String, promoCode: String): Result<Unit>
-}
