f0496c7311ec54d000bd7803dfa68f4761e4ae90
velvet <berhram@proton.me>
2023-09-06 17:28:37 +0300

    feat: deleted sber service

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/sber/model/GetOrderStatusResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.progressterra.ipbandroidapi.api.sber.model
-
-import com.google.gson.annotations.SerializedName
-import com.progressterra.ipbandroidapi.base.BaseResponse
-
-internal data class GetOrderStatusResponse(
-    @SerializedName("orderStatus") val orderStatus: OrderStatus
-) : BaseResponse()
