718d2551d8afc94f3b728ec614e1b3b0870edee4
velvet <berhram@proton.me>
2023-09-06 17:22:23 +0300

    feat: deleted cooperation service

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/typecooperation/TypeCooperationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.progressterra.ipbandroidapi.api.typecooperation
-
-import com.progressterra.ipbandroidapi.api.typecooperation.model.RFTypeCooperationData
-
-interface TypeCooperationRepository {
-
-    suspend fun getCooperationTypeList(accessToken: String): Result<List<RFTypeCooperationData>>
-}
--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/typecooperation/model/CooperationType.kt
+++
@@ -1,7 +0,0 @@
-package com.progressterra.ipbandroidapi.api.typecooperation.model
-
-enum class CooperationType {
-    SELF_EMPLOYER,
-    DIRECT_INCOME,
-    OTHER
-}
