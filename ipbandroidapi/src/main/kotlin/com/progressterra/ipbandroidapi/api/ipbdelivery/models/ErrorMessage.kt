e19d5a74b31aa544f361e54bb7270d89151b4d22
velvet <berhram@proton.me>
2023-09-06 17:49:06 +0300

    feat: deleted delivery service

--- ipbandroidapi/src/main/kotlin/com/progressterra/ipbandroidapi/api/ipbdelivery/models/ErrorMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.progressterra.ipbandroidapi.api.ipbdelivery.models
-
-import com.google.gson.annotations.SerializedName
-
-data class ErrorMessage(
-
-    @SerializedName("message")
-    val message: String? = null
-)
