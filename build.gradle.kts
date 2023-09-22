93456a33a42cb68b823506ca2f20033a5353d6a8
velvet <berhram@proton.me>
2023-09-22 12:53:34 +0300

    build: ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
 allprojects {
     group = "com.progressterra.ipbandroidapi"
-    version = "0.5.52"
+    version = "0.5.53"
     repositories {
         google()
         mavenCentral()
