9298f06f9abbcf4b36fa540e8395334e802e059d
Daniíl Pechorin <berhram@protonmail.com>
2023-10-04 11:29:23 +0300

    build: ver bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
 allprojects {
     group = "com.progressterra.ipbandroidapi"
-    version = "0.5.54"
+    version = "0.5.55"
     repositories {
         google()
         mavenCentral()
