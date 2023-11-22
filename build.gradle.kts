1a3256747f1249eb7a420e041f9e6d12501ed6e8
Daniíl Pechorin <berhram@protonmail.com>
2023-11-23 02:16:45 +0300

    build: ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
 allprojects {
     group = "com.progressterra.ipbandroidapi"
-    version = "1.1.6"
+    version = "1.1.12"
     repositories {
         google()
         mavenCentral()
